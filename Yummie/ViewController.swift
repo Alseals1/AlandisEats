e36345b4fc46ecc0de3748c7ca2a69069e18ca33
alseals1 <1seals.alandis@gmail.com>
2022-02-02 08:41:10 -0600

    Initial Commit

---
+++ Yummie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yummie
+//
+//  Created by Alandis Seals on 2/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
